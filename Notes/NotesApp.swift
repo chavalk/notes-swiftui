a89d4b96a0dca73a7e751da073781cbc38d41142
chavalk <chavalk@hotmail.com>
2023-03-06 21:43:21 -0600

    First commit.

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Jose Garcia on 3/6/23.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
