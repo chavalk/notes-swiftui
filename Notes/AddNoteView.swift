932485342ffac2940f183d24fc15a41699e3e157
chavalk <chavalk@hotmail.com>
2023-03-07 16:01:10 -0600

    Add state var for text.

--- Notes/AddNoteView.swift
+++ Notes/AddNoteView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct AddNoteView: View {
+    
+    @State var text = ""
+    
     var body: some View {
         HStack {
             TextField("Write a note...", text: <#T##Binding<String>#>)
