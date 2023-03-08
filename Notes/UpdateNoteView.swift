cc24939a5dc5620ac236512ee30b85cfc1e85592
chavalk <chavalk@hotmail.com>
2023-03-07 20:57:21 -0600

    Modify update note function to make actual call to database to update note.

--- Notes/UpdateNoteView.swift
+++ Notes/UpdateNoteView.swift
@@ -25,7 +25,45 @@ struct UpdateNoteView: View {
     }
     
     func updateNote() {
-        print("UPDATE")
+        let params = ["note" : text] as [String : Any]
+        
+        let url = URL(string: "http://localhost:3000/notes/\(noteId)")!
+        
+        let session = URLSession.shared
+        
+        var request = URLRequest(url: url)
+        
+        request.httpMethod = "POST"
+        
+        do {
+            request.httpBody = try JSONSerialization.data(withJSONObject: params, options: .prettyPrinted)
+        }
+        catch let error {
+            print(error)
+        }
+        
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        
+        let task = session.dataTask(with: request) { data, res, err in
+            guard err == nil else { return }
+            
+            guard let data = data else { return }
+            
+            do {
+                if let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] {
+                    print(json)
+                }
+            }
+            catch let error {
+                print(error)
+            }
+        }
+        
+        task.resume()
+        
+        self.text = ""
+        presentationMode.wrappedValue.dismiss()
     }
 }
 
